d74a38760821826093c9e023dab9e227749ef13e
Christoph Dorn <christoph@christophdorn.com>
2017-11-16 21:43:50 -0800

    wip

--- interface.js
+++ interface.js
@@ -1,19 +1,23 @@
 #!/usr/bin/env node
 
 const PATH = require("path");
+const FS = require("fs");
+
+const BASE_PATH = PATH.dirname(FS.realpathSync(__filename));
 
 
 console.log("[interface.js] __dirname", __dirname);
 console.log("[interface.js] PWD", process.cwd);
+console.log("[interface.js] BASE_PATH", BASE_PATH);
 
 
 exports.version = require(PATH.join(
-    __dirname,
+    BASE_PATH,
     "package.json"
 )).version;
 
 exports.node_modules = PATH.join(
-    __dirname,
+    BASE_PATH,
     "dependencies/.node-" + process.version.split(".")[0],
     "node_modules"
 );
