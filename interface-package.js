b9c74bb3befce167f5410beaa0ee632a51bb27c8
Christoph Dorn <christoph@christophdorn.com>
2017-12-04 20:07:22 -0800

    wip

--- interface-package.js
+++ interface-package.js
@@ -2,15 +2,14 @@
 const PATH = require("path");
 const FS = require("fs");
 
-module.exports = {
-
-    get LIB () {
-        var basePath = __dirname;
+function requireCommonInterface (basePath) {
+    if (!basePath) {
+        basePath = __dirname;
         var path = null;
         while (true) {
             path = PATH.join(basePath, "node_modules/.bin/bash.origin.workspace.inf.js");
             if (FS.existsSync(path)) {
-                return require(path).LIB;
+                break;
             }
             path = basePath;
             basePath = PATH.dirname(basePath);
@@ -18,9 +17,25 @@ module.exports = {
                 throw new Error("[bash.origin.workspace] While resolving 'LIB' cannot find 'node_modules/.bin/bash.origin.workspace.inf.js' from '" + __dirname + "' nor by walking up the tree!");
             }
         }
+    }
+    return require(PATH.join(basePath, "node_modules/.bin/bash.origin.workspace.inf.js"));
+}
+
+module.exports = {
+
+    get version () {
+        return requireCommonInterface().version;
+    },
+
+    get node_modules () {
+        return requireCommonInterface().node_modules;
+    },
+
+    get LIB () {
+        return requireCommonInterface().LIB;
     },
 
     forPackage: function (basePath) {
-        return require(PATH.join(basePath, "node_modules/.bin/bash.origin.workspace.inf.js")).forPackage(basePath);
+        return requireCommonInterface(basePath).forPackage(basePath);
     }
 };
